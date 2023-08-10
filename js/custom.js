4851f3c0f28c385c63566ba40e139087fa7a9d8d
Sabbir Hasan Asad <117185573+SabbirHasan280@users.noreply.github.com>
2023-08-11 02:32:41 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+//magnific popup box start//
+
+$(document).ready(function() {
+  $('.gallerys').magnificPopup({
+      type:'image',
+      delegate: 'a',
+      gallery:{enabled:true},
+  });
+});
+
+//magnific popup box end//
